a068879e17b5ef0cd0367dff27add44979449b86
Roman <romeenice@gmail.com>
2023-02-10 21:15:21 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,5 +5,26 @@ export default class App extends React.Component {
     neutral: 0,
     bad: 0,
   };
-  render() {}
+  render() {
+    return (
+      <div>
+        <h1>Give feedback</h1>
+        <button onClick={() => this.setState({ good: this.state.good + 1 })}>
+          Good
+        </button>
+        <button
+          onClick={() => this.setState({ neutral: this.state.neutral + 1 })}
+        >
+          Neutral
+        </button>
+        <button onClick={() => this.setState({ bad: this.state.bad + 1 })}>
+          Bad
+        </button>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
 }
