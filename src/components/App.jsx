8be7a819ea9c4d0b5b4f667a9f7f1de97f70fef1
Roman <romeenice@gmail.com>
2023-02-10 21:03:01 +0200

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+export default class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {}
+}
