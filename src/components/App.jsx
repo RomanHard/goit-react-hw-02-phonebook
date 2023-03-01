d557c3558dc85fffa6bd75e0c3b19248c1020ae5
Roman <romeenice@gmail.com>
2023-03-01 22:48:59 +0200

    summary 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,8 @@ import { nanoid } from 'nanoid';
 
 export default class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
-    name: '',
-    number: '',
   };
 
   handleChange = (e) => {
@@ -23,82 +16,104 @@ export default class App extends React.Component {
     return (
       <form className="form_feedback">
         <h1 className="title">Phonebook</h1>
-        <div className="container">
-          <h3 className="title">Name</h3>
-          <input
-            className="input_name"
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <h3 className="title">Number</h3>
-          <input
-            className="input_number"
-            type="tel"
-            name="number"
-            value={this.state.number}
-            onChange={this.handleChange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-          <button
-            className="button"
-            onClick={(e) => {
-              e.preventDefault();
-              const existingContact = this.state.contacts.find(
-                (contact) => contact.name === this.state.name
-              );
-
-              if (existingContact) {
-                alert('Такой контакт вже існує!');
-                return;
-              }
-              this.setState({
-                contacts: [
-                  ...this.state.contacts,
-                  {
-                    id: nanoid(),
-                    name: this.state.name,
-                    number: this.state.number,
-                  },
-                ],
-                name: '',
-                number: '',
-              });
-            }}
-          >
-            Add contact
-          </button>
-        </div>
+        <ContactForm />
         <div className="container_render">
           <h2>Contacts</h2>
           <p>Find contacts by name</p>
-          <input
-            className="input_filter"
-            type="text"
-            name="filter"
-            value={this.state.filter}
-            onChange={this.handleChange}
-          />
-
-          <ul>
-            {this.state.contacts
-              .filter((contact) =>
-                contact.name.toLowerCase().includes(this.state.filter)
-              )
-              .map((contact) => (
-                <li key={contact.id}>
-                  {contact.name}: {contact.number}
-                </li>
-              ))}
-          </ul>
+          <Filter />
+          <ContactList />
         </div>
       </form>
     );
+  }
+}
+
+class ContactForm extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  render() {
+    <div className="container">
+      <h3 className="title">Name</h3>
+      <input
+        className="input_name"
+        type="text"
+        name="name"
+        value={this.state.name}
+        onChange={this.handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <h3 className="title">Number</h3>
+      <input
+        className="input_number"
+        type="tel"
+        name="number"
+        value={this.state.number}
+        onChange={this.handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <button
+        className="button"
+        onClick={(e) => {
+          e.preventDefault();
+          const existingContact = this.state.contacts.find(
+            (contact) => contact.name === this.state.name
+          );
+
+          if (existingContact) {
+            alert('Такой контакт вже існує!');
+            return;
+          }
+          this.setState({
+            contacts: [
+              ...this.state.contacts,
+              {
+                id: nanoid(),
+                name: this.state.name,
+                number: this.state.number,
+              },
+            ],
+            name: '',
+            number: '',
+          });
+        }}
+      >
+        Add contact
+      </button>
+    </div>;
+  }
+}
+
+class ContactList extends React.Component {
+  render() {
+    <ul>
+      {this.state.contacts
+        .filter((contact) =>
+          contact.name.toLowerCase().includes(this.state.filter)
+        )
+        .map((contact) => (
+          <li key={contact.id}>
+            {contact.name}: {contact.number}
+          </li>
+        ))}
+    </ul>;
+  }
+}
+
+class Filter extends React.Component {
+  render() {
+    <input
+      className="input_filter"
+      type="text"
+      name="filter"
+      value={this.state.filter}
+      onChange={this.handleChange}
+    />;
   }
 }
