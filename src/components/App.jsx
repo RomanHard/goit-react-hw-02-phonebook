1346270b00da0fa1d26057de95556f6de7bc16ff
Roman <romeenice@gmail.com>
2023-03-01 23:18:00 +0200

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,48 +7,49 @@ export default class App extends React.Component {
     filter: '',
   };
 
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
+  handleFilterChange = (filter) => {
+    this.setState({ filter });
   };
 
-  handleNameChange = (e) => {
-    this.setState({ name: e.target.value });
-  };
+  handleAddContact = (contact) => {
+    const existingContact = this.state.contacts.find(
+      (c) => c.name === contact.name
+    );
 
-  handleNumberChange = (e) => {
-    this.setState({ number: e.target.value });
-  };
+    if (existingContact) {
+      alert('Такий контакт вже існує!');
+      return;
+    }
 
-  handleDelete = (id) => {
     this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  handleDeleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((c) => c.id !== id),
     }));
   };
 
   render() {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter((c) =>
+      c.name.toLowerCase().includes(normalizedFilter)
+    );
+
     return (
       <form className="form_feedback">
         <h1 className="title">Phonebook</h1>
-        <ContactForm
-          contacts={this.state.contacts}
-          onAddContact={(contact) =>
-            this.setState({
-              contacts: [...this.state.contacts, contact],
-            })
-          }
-        />
+        <ContactForm onAddContact={this.handleAddContact} />
         <div className="container_render">
           <h2>Contacts</h2>
           <p>Find contacts by name</p>
-          <Filter
-            filter={this.state.filter}
-            onFilterChange={(filter) => this.setState({ filter })}
-          />
+          <Filter filter={filter} onFilterChange={this.handleFilterChange} />
           <ContactList
-            contacts={this.state.contacts}
-            filter={this.state.filter}
-            onDelete={this.handleDelete}
+            contacts={filteredContacts}
+            onDeleteContact={this.handleDeleteContact}
           />
         </div>
       </form>
@@ -62,37 +63,25 @@ class ContactForm extends React.Component {
     number: '',
   };
 
-  handleChange = (e) => {
+  handleInputChange = (e) => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const existingContact = this.props.contacts.find(
-      (contact) => contact.name === this.state.name
-    );
 
-    if (existingContact) {
-      alert('Такой контакт вже існує!');
-      return;
-    }
-
-    const contact = {
-      id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
-    };
+    const { name, number } = this.state;
+    const contact = { id: nanoid(), name, number };
 
     this.props.onAddContact(contact);
 
-    this.setState({
-      name: '',
-      number: '',
-    });
+    this.setState({ name: '', number: '' });
   };
 
   render() {
+    const { name, number } = this.state;
+
     return (
       <div className="container">
         <h3 className="title">Name</h3>
@@ -100,8 +89,8 @@ class ContactForm extends React.Component {
           className="input_name"
           type="text"
           name="name"
-          value={this.state.name}
-          onChange={this.handleChange}
+          value={name}
+          onChange={this.handleInputChange}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters and apostrophes, and must start and end with a letter."
           required
@@ -111,8 +100,8 @@ class ContactForm extends React.Component {
           className="input_number"
           type="tel"
           name="number"
-          value={this.state.number}
-          onChange={this.handleChange}
+          value={number}
+          onChange={this.handleInputChange}
           pattern="^[0-9]{3}-[0-9]{2}-[0-9]{2}$"
           title="Number must be in format 000-00-00"
           required
@@ -125,43 +114,34 @@ class ContactForm extends React.Component {
   }
 }
 
-class Filter extends React.Component {
-  render() {
-    return (
-      <input
-        className="input_filter"
-        type="text"
-        name="filter"
-        value={this.props.filter}
-        onChange={(e) => this.props.onFilterChange(e.target.value)}
-      />
-    );
-  }
+function Filter({ filter = '', onFilterChange }) {
+  return (
+    <input
+      className="input_filter"
+      type="text"
+      value={filter}
+      onChange={(e) => onFilterChange(e.target.value)}
+    />
+  );
 }
 
-class ContactList extends React.Component {
-  deleteContact = (id) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => contact.id !== id),
-    }));
-  };
-
-  render() {
-    const { contacts, filter } = this.props;
-    const normalizedFilter = filter.toLowerCase();
-    const filteredContacts = contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-
-    return (
-      <ul className="list">
-        {filteredContacts.map((contact) => (
-          <li key={contact.id} className="item">
+function ContactList({ contacts, onDeleteContact }) {
+  return (
+    <ul className="list">
+      {contacts.map((contact) => (
+        <li key={contact.id} className="item">
+          <p className="text">
             {contact.name}: {contact.number}
-            <button className="button_delete">Delete</button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+          </p>
+          <button
+            className="button_delete"
+            type="button"
+            onClick={() => onDeleteContact(contact.id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
